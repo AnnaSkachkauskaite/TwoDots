1b409602e69554f6819e7f4f831eba2d08eed305
GalinaSazonova <galka.saz@yandex.ru>
2014-10-31 21:58:45 +0300

    Back buttons

--- TwoDots1/gen/ru/startandroid/develop/twodots1/R.java
+++ TwoDots1/gen/ru/startandroid/develop/twodots1/R.java
@@ -14,24 +14,32 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Button01=0x7f060009;
-        public static final int Button02=0x7f06000a;
-        public static final int Button03=0x7f06000b;
-        public static final int Button04=0x7f060007;
-        public static final int Button05=0x7f060006;
-        public static final int Button06=0x7f060004;
-        public static final int Button07=0x7f060005;
-        public static final int TableRow01=0x7f060003;
-        public static final int button04=0x7f06000c;
-        public static final int button1=0x7f06000d;
-        public static final int levelText=0x7f060002;
-        public static final int tableRow1=0x7f060000;
-        public static final int tableRow2=0x7f060008;
-        public static final int textView1=0x7f060001;
+        public static final int Button01=0x7f06000b;
+        public static final int Button02=0x7f06000c;
+        public static final int Button03=0x7f06000d;
+        public static final int Button04=0x7f06000e;
+        public static final int Button05=0x7f060010;
+        public static final int Button06=0x7f060011;
+        public static final int Button07=0x7f060012;
+        public static final int TableRow2=0x7f06000a;
+        public static final int button08=0x7f060013;
+        public static final int button1=0x7f060014;
+        public static final int buttonBackToMainMenu=0x7f060009;
+        public static final int buttonBackToMenu=0x7f060004;
+        public static final int levelText=0x7f060006;
+        public static final int levelText1=0x7f060008;
+        public static final int tableRow1=0x7f060005;
+        public static final int tableRow3=0x7f06000f;
+        public static final int tableRow4=0x7f060007;
+        public static final int textView1=0x7f060000;
+        public static final int textView2=0x7f060001;
+        public static final int textView3=0x7f060002;
+        public static final int textView4=0x7f060003;
     }
     public static final class layout {
-        public static final int levelmenu=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int level1=0x7f030000;
+        public static final int levelmenu=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
